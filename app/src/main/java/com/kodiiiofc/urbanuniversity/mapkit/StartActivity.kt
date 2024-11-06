9e19b4019d2510bdd1f446f5fbf8ae399b4f3e80
Roman Mokhnatkin <kodiiiofc@gmail.com>
2024-11-07 00:26:31 +0500

    Домашнее задание по теме "MapKit"

--- app/src/main/java/com/kodiiiofc/urbanuniversity/mapkit/StartActivity.kt
+++ app/src/main/java/com/kodiiiofc/urbanuniversity/mapkit/StartActivity.kt
@@ -1,6 +1,7 @@
 package com.kodiiiofc.urbanuniversity.mapkit
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
@@ -11,8 +12,10 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
+import androidx.lifecycle.DefaultLifecycleObserver
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -24,16 +27,30 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        getCurrentLocation()
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val permissionsLocation = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            permissionsRequest.launch(permissionsLocation)
+        } else {
+            getCurrentLocation()
+        }
+
+
+
         GlobalScope.launch(Dispatchers.Main) {
             delay(5000)
             val intent = Intent(this@StartActivity, MainActivity::class.java)
                 .apply {
-                putExtras(bundleOf("lat" to location?.latitude, "lon" to location?.longitude))
-            }
+                    putExtras(bundleOf("lat" to location?.latitude, "lon" to location?.longitude))
+                }
             startActivity(intent)
+            finish()
         }
-
 
 
     }
@@ -47,30 +64,22 @@ class StartActivity : AppCompatActivity() {
         }
         if (allAreGranted) {
             Toast.makeText(this@StartActivity, "Разрешения предоставлены", Toast.LENGTH_LONG)
+            getCurrentLocation()
         } else {
             Toast.makeText(this@StartActivity, "В разрешениях отказано", Toast.LENGTH_LONG)
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun getCurrentLocation() {
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-            checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        locationManager!!.requestLocationUpdates(
+            LocationManager.NETWORK_PROVIDER,
+            500,
+            200f,
         ) {
-            locationManager!!.requestLocationUpdates(
-                LocationManager.NETWORK_PROVIDER,
-                500,
-                200f,
-            ) {
-                location = it
-                Log.d("aaa", "getCurrentLocation: ${it.longitude} , ${it.latitude}")
-            }
-        } else {
-            val permissionsLocation = arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-            permissionsRequest.launch(permissionsLocation)
+            location = it
+            Log.d("aaa", "getCurrentLocation: ${it.longitude} , ${it.latitude}")
         }
     }
 
